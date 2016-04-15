6cb2b4a6a3e0c7955577e0dec5007844fbec4a71
Josh Girvin <josh@studionone.com.au>
2016-04-15 11:47:29 +1000

    Initial yargs integration

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,26 @@
 module Main where
 
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Prelude (Unit, return, (<$>), (<*>), ($), (<>), unit)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Data.Maybe
+import Data.Array (reverse)
+import Data.Either
+
+import Node.Yargs.Setup
+import Node.Yargs.Applicative
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log, print)
+import Control.Monad.Eff.Exception (EXCEPTION())
+
+app :: forall eff. Array String -> Boolean -> Eff (console::CONSOLE | eff) Unit
+app [] _ = return unit            -- No-op
+app ss false = print ss           -- Not sure...
+app ss true = print (reverse ss)  -- Hmm...
+
+main :: forall eff. Eff (console::CONSOLE, err::EXCEPTION | eff) Unit
 main = do
-  log "Hello sailor!"
+  let setup = usage "$0 -w Word1 -w Word2"
+              <> example "$0 -w Hello -w World" "Say hello!"
+  runY setup $ app <$> yarg "w" ["word"] (Just "A word") (Right "At least one word is required") false
+                   <*> flag "r" []       (Just "Reverse the words")
